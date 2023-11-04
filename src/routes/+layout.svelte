99870552fef104ff596c4cc760f194c1eff5b244
julianvansanten <julian@julianvansanten.nl>
2023-11-04 17:06:11 +0100

    Prettify front end

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,15 @@
 
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom'
-	import { storePopup } from '@skeletonlabs/skeleton'
+	import { AppShell, storePopup } from '@skeletonlabs/skeleton'
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow })
 </script>
 
 <div class="main">
-	<div class="container h-full mx-auto flex justify-center items-center">
-		<slot />
-	</div>
+	<AppShell>
+		<svelte:fragment slot="header"></svelte:fragment>
+		<div class="container h-full mx-auto flex justify-center items-center my-4">
+			<slot />
+		</div>
+	</AppShell>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,5 +8,8 @@
 <div class="space-y-5">
 	<h1 class="h1 text-center">Choose your favorite posters!</h1>
 	<h2 class="h2 italic text-center">The best poster gets a cream cake!</h2>
-	<PosterForm groups={data.groups} />
+
+	<div class="p-4">
+		<PosterForm groups={data.groups} />
+	</div>
 </div>
