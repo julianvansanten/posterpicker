0ec4ecc56df1bd7e1f35a37879bca4fbcad03094
julianvansanten <julian+github@julianvansanten.nl>
2023-11-06 21:17:57 +0100

    Add AppBar to admin panel

---
+++ src/routes/admin/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Icon from '@iconify/svelte'
+	import { AppBar } from '@skeletonlabs/skeleton'
+</script>
+
+<div>
+	<AppBar>
+		<svelte:fragment slot="lead"><Icon icon="ep:back" class="h-6 w-6" /></svelte:fragment>
+		<h1>Poster Picker Admin</h1>
+		<svelte:fragment slot="trail">(actions)</svelte:fragment>
+	</AppBar>
+    <slot />
+</div>
