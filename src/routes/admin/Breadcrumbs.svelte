a5337c46c9f7f6621bc7afeca3f123a583291a55
julianvansanten <julian+github@julianvansanten.nl>
2023-11-19 12:29:28 +0100

    Add Breadcrumbs stub component

---
+++ src/routes/admin/Breadcrumbs.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    type Breadcrumb = {
+        label: string
+        href: string
+    }
+    const breadcrumbs: Breadcrumb[] = []
+</script>
+
+<ol class="breadcrumb">
+    {#each breadcrumbs as crumb, i}
+        {#if i < breadcrumbs.length - 1}
+            <!-- Each crumb has link and a separator follows -->
+            <li class="crumb"><a class="anchor" href="{crumb.href}">{crumb.label}</a></li>
+            <li class="crumb-separator"></li>
+        {:else}
+            <!-- Last crumb, does not have a link -->
+            <li class="crumb">{crumb.label}</li>
+        {/if}
+        
+    {/each}
+</ol>
