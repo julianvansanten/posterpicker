0906ad2f8c747b0bbb5e9e1a1f2a033850becb17
julianvansanten <julian@julianvansanten.nl>
2024-04-10 13:06:50 +0200

    Remove logout stub

--- src/routes/admin/logout/+server.ts
+++
@@ -1,9 +0,0 @@
-import { json, fail, type RequestHandler } from '@sveltejs/kit'
-
-export const POST = async ({ locals }) => {
-	if (!locals.pb.authStore.isValid) {
-		throw fail(400, { message: 'Not logged in' })
-	}
-	locals.pb.authStore.clear()
-	return json({ message: 'Logged out' })
-}
